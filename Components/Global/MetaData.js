d6206a3822a7169a9bf6a098cc082c312e1907c6
ryanbranco <ryanbr23@gmail.com>
2021-03-29 10:13:44 -0700

    change title

--- Components/Global/MetaData.js
+++ Components/Global/MetaData.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function MetaData(props) {
     return (
         <Head>
-            <title>{props.title} - Petmatcher</title>
+            <title>{props.title} &#xb7; Petmatcher</title>
             <link rel="icon" href="/favicon.ico" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&display=swap" rel="stylesheet" /> 
