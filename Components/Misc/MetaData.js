394cf46ce0f9e3024c75472d4e284f3e51718ac7
ryanbranco <ryanbr23@gmail.com>
2021-04-02 16:03:54 -0700

    apple-touch-icon

--- Components/Misc/MetaData.js
+++ Components/Misc/MetaData.js
@@ -7,7 +7,7 @@ export default function MetaData(props) {
             <meta name="theme-color" content="#37eeff" />
 
             <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1" />
-            <link rel="apple-touch-icon" href="/manifestLogo512x512.png" />
+            <link rel="apple-touch-icon" href="/manifest-logo512x512.png" />
             <meta name="description" content="The Internet portal for cuteness!" />
             <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500&display=swap" rel="stylesheet" />
             <title>{props.title} &#xb7; Petmatcher</title>
