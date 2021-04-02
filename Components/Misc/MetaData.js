d37166d4e4f9d11b1bc845738f10d88c86b575db
ryanbranco <ryanbr23@gmail.com>
2021-04-02 16:19:41 -0700

    manifest color change + code cleanup

--- Components/Misc/MetaData.js
+++ Components/Misc/MetaData.js
@@ -3,18 +3,14 @@ import Head from "next/head"
 export default function MetaData(props) {
     return (
         <Head>
-            <link rel="manifest" href="/manifest.json" />
-            <meta name="theme-color" content="#37eeff" />
-
             <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1" />
-            <link rel="apple-touch-icon" href="/manifest-logo512x512.png" />
-            <meta name="description" content="The Internet portal for cuteness!" />
-            <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500&display=swap" rel="stylesheet" />
+            <meta name="description" content={props.description} />
             <title>{props.title} &#xb7; Petmatcher</title>
         </Head>
     )
 }
 
 MetaData.defaultProps = {
-    title: "Home"
+    title: "Home",
+    description: "The Internet portal for cuteness!"
 }
