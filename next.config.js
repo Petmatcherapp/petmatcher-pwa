14aaab6c2c94f2e831084ff34c5b0a3b9d123191
ryanbranco <ryanbr23@gmail.com>
2021-04-01 21:20:22 -0700

    switch svgr - webpack to next-images

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 // next.config.js
     
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
-};
+const withImages = require('next-images')
+module.exports = withImages()
