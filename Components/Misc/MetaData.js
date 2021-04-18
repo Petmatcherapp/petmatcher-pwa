3e41300062d83ee75ea59073c6e6a80a4c018d77
ryanbranco <ryanbr23@gmail.com>
2021-04-18 13:15:58 -0700

    V

--- Components/Misc/MetaData.js
+++ Components/Misc/MetaData.js
@@ -3,10 +3,6 @@ import Head from "next/head"
 export default function MetaData(props) {
     return (
         <Head>
-            {/* <link rel="manifest" href="/manifest.json" />
-            <meta name="theme-color" content="#37eeff" />
-            <link rel="apple-touch-icon" href="/manifest/manifest-logo512x512.png" /> */}
-
             <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1" />
             <meta name="description" content={props.description} />
             <title>{props.title} &#xb7; Petmatcher</title>
