fd79ed75c5f52accec669201dd5d05635be71bed
ryanbranco <ryanbr23@gmail.com>
2021-04-18 12:23:10 -0700

    link apple-touch-icon in MetaData.js

--- Components/Misc/MetaData.js
+++ Components/Misc/MetaData.js
@@ -3,6 +3,10 @@ import Head from "next/head"
 export default function MetaData(props) {
     return (
         <Head>
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="theme-color" content="#37eeff" />
+            <link rel="apple-touch-icon" href="/manifest-logo512x512.png" />
+            
             <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1" />
             <meta name="description" content={props.description} />
             <title>{props.title} &#xb7; Petmatcher</title>
