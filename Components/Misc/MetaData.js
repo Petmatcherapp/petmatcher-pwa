7edb0d7936a80e65e5ed1752761386ba5d98eb39
ryanbranco <ryanbr23@gmail.com>
2021-04-18 12:17:04 -0700

    change apple-touch-icon

--- Components/Misc/MetaData.js
+++ Components/Misc/MetaData.js
@@ -3,14 +3,6 @@ import Head from "next/head"
 export default function MetaData(props) {
     return (
         <Head>
-            <link rel="manifest" href="/manifest.json" />
-            <meta name="theme-color" content="#583eff" />
-            <meta name='application-name' content='Petmatcher' />
-            <meta name='apple-mobile-web-app-capable' content='yes' />
-            <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-            <meta name='apple-mobile-web-app-title' content='Petmatcher' />
-            <link rel="apple-touch-icon" href="/manifest-logo512x512.png" />
-            
             <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1" />
             <meta name="description" content={props.description} />
             <title>{props.title} &#xb7; Petmatcher</title>
