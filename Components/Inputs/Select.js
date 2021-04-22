57a59d9c206348dee0727ed3e8332e237f7ba17f
ryanbranco <ryanbr23@gmail.com>
2021-04-22 09:27:07 -0700

    shadow adjustments

--- Components/Inputs/Select.js
+++ Components/Inputs/Select.js
@@ -15,23 +15,25 @@ export default function Select({ value, id, onChange }) {
         <div className="display-inline-flex align-end">
             <div className="display-inline-flex-column">
                 <label className="bottom-margin-extra-small" htmlFor={id}><small>{id}</small></label>
-                <div className="display-inline-flex gradient-2 padding-2 border-radius-12 clickable-shadow">
-                    <select value={value} onChange={handleChange} onFocus={handleFocus} id={id} className={`${styles.select} width-100 height-100 background-white border-radius-10 padding-5 padding-left-right-10`}>
-                        <optgroup label="common pets">
-                            <option value="dog">Dog</option>
-                            <option value="cat">Cat</option>
-                        </optgroup>
-                        <optgroup label="else">
-                            <option value="lizard">lizard</option>
-                            <option value="horse">horse</option>
-                            <option value="hamster">Hamster</option>
-                        </optgroup>
-                    </select>
+                <div className="display-inline-flex align-center">
+                    <div className="display-inline-flex gradient-2 padding-2 border-radius-12 clickable-shadow">
+                        <select value={value} onChange={handleChange} onFocus={handleFocus} id={id} className={`${styles.select} width-100 height-100 background-white border-radius-10 padding-5 padding-left-right-10`}>
+                            <optgroup label="common pets">
+                                <option value="dog">Dog</option>
+                                <option value="cat">Cat</option>
+                            </optgroup>
+                            <optgroup label="else">
+                                <option value="lizard">lizard</option>
+                                <option value="horse">horse</option>
+                                <option value="hamster">Hamster</option>
+                            </optgroup>
+                        </select>
+                    </div>
+                    <span className={`${styles.downArrowContainer} display-flex-row align-center`}>
+                        <DownArrow height={10} />
+                    </span>
                 </div>
             </div>
-            <span className={`${styles.downArrowContainer} display-flex-row align-center`}>
-                <DownArrow height={20} />
-            </span>
         </div>
     )
 }
--- Components/PageSpecific/CreateNewDiv.js
+++ Components/PageSpecific/CreateNewDiv.js
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react"
+import clickSound from "../../audio/buttonClick1.mp3"
 import Link from "next/link"
 import styles from "../../styles/components/pageSpecific/CreateNewDiv.module.css"
 
 export default function CreateNewDiv({ icon, type, description, link, gradientNum}) {
+    // set sound on server
+    const [sound, setSound] = useState(null)
+    
+    useEffect(() => {
+        // set sound on client
+        setSound(new Audio(clickSound))
+    }, [])
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        sound.play()
+    }
+
     return (
         <Link href={link}>
-            <div className={`${styles.outerCreateNewDiv} gradient-${gradientNum} border-radius-20 bottom-margin-large clickable-shadow padding-2 cursor-pointer`}>
+            <div onClick={handleClick} className={`${styles.outerCreateNewDiv} gradient-${gradientNum} border-radius-20 bottom-margin-large clickable-shadow padding-2 cursor-pointer`}>
                     <div className={`${styles.innerCreateNewDiv} display-flex-row border-radius-18`}>
                         <div className={`${styles.typeContainer} display-flex-column align-center justify-center`}>
                             <div className={`${styles.iconContainer} bottom-margin-small`}>
