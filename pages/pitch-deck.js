7d3de52c0b58825056761c8b774bfa2184e2504f
ryanbranco <ryanbr23@gmail.com>
2021-04-28 12:16:02 -0700

    polish link

--- pages/pitch-deck.js
+++ pages/pitch-deck.js
@@ -1,12 +1,14 @@
 function pitchDeck() {
     return (
-        <div style={{height: "100vh"}} className="display-flex-row justify-center align-center">
+        <main style={{height: "100vh"}} className="display-flex-row justify-center align-center">
             <h1 className="display-flex-row align-center">
-                <a className="gradient-text gradient-3" href="/petmatcher-pitch-deck-compressed.pdf">View the Petmatcher pitch deck</a>
-                &nbsp;
-                <span>😸</span>
+                <a href="/petmatcher-pitch-deck-compressed.pdf">
+                    <span className="gradient-text gradient-3">View the Petmatcher pitch deck</span>
+                    &nbsp;
+                    <span>😸</span>
+                </a>
             </h1>
-        </div>
+        </main>
     )
 }
 
