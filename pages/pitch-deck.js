ab2842e37e1cebdb19ac12d3481cce4f2b29e906
ryanbranco <ryanbr23@gmail.com>
2021-04-28 12:31:56 -0700

    fix cutoff link bug

--- pages/pitch-deck.js
+++ pages/pitch-deck.js
@@ -1,13 +1,15 @@
 function pitchDeck() {
     return (
         <main style={{height: "100vh"}} className="display-flex-row justify-center align-center">
-            <h2 className="display-flex-row align-center">
-                <a href="/petmatcher-pitch-deck-compressed.pdf">
-                    <span className="gradient-text gradient-3">View the Petmatcher pitch deck</span>
-                    &nbsp;
-                    <span>😸</span>
-                </a>
-            </h2>
+            <div className="display-flex-row align-center">
+                <p>
+                    <a href="/petmatcher-pitch-deck-compressed.pdf">
+                        <span className="gradient-text gradient-3">View the Petmatcher pitch deck</span>
+                        &nbsp;
+                        <span>😸</span>
+                    </a>
+                </p>
+            </div>
         </main>
     )
 }
