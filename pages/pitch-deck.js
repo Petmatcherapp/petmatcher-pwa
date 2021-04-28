856392967b01f9dd7f3abc0b068f5570a747fba7
ryanbranco <ryanbr23@gmail.com>
2021-04-28 10:55:43 -0700

    host pitch deck

---
+++ pages/pitch-deck.js
@@ -0,0 +1,9 @@
+function pitchDeck() {
+    return (
+        <div style={{height: "100vh"}}>
+            <embed src="../petmatcher-pitch-deck.pdf" width="100%" height="100%"></embed>
+        </div>
+    )
+}
+
+export default pitchDeck
