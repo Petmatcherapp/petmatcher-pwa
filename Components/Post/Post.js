0a14437559c5e54f578620493ea5a95740867ae4
ryanbranco <ryanbr23@gmail.com>
2021-05-07 16:37:30 -0700

    award background

--- Components/Post/Post.js
+++ Components/Post/Post.js
@@ -26,16 +26,16 @@ export default function Post({ postData }) {
     }
 
     return (
-        <div className={`${styles.postContainer} border-radius-20 display-flex-column normal-shadow`}>
+        <div className={`${styles.postContainer} border-radius-10 display-flex-column normal-shadow`}>
             {postData.srcType === "video" &&
-            <video controls className={`${styles.video} border-radius-20 width-100`} src={postData.src} alt={postData.description}></video>}
+            <video controls className={`${styles.video} border-radius-10 width-100`} src={postData.src} alt={postData.description}></video>}
             {postData.srcType === "image" &&
-            <img className={`${styles.image} border-radius-20 width-100`} src={postData.src} alt={postData.description}></img>}
+            <img className={`${styles.image} border-radius-10 width-100`} src={postData.src} alt={postData.description}></img>}
             <div onClick={() => handlePostClick("overlay")} className={`${styles.postUiOverlay} ${overlayVisibility} justify-between height-100 width-100 padding-5 padding-top-bottom-10`}>
                 <PostHeader animal={postData.animal} />
                 <small className="color-white weight-700 text-shadow-black-reverse margin-0 padding-left-right-10">{postData.description}</small>
             </div>
-            <div onClick={() => handlePostClick("actions")} className={`${styles.postActionsContainer} ${actionsVisibility} height-100 width-100 border-radius-20`}>
+            <div onClick={() => handlePostClick("actions")} className={`${styles.postActionsContainer} ${actionsVisibility} height-100 width-100 border-radius-10`}>
                 <PostActions />
             </div>
         </div>
--- Components/Post/PostActions.js
+++ Components/Post/PostActions.js
@@ -1,11 +1,13 @@
 import { useState } from "react"
 import CatLove from "../Images/Misc/CatLove"
 import CatLaugh from "../Images/Misc/CatLaugh"
+import AwardBackground from "../Images/Svgs/Misc/AwardBackground"
 import styles from "../../styles/components/post/postAction.module.css"
 
 export default function PostActions({ postMetaData }) {
     const [love, setLove] = useState(`${styles.reactionLoveHover} ${styles.translateLoveStart} cursor-pointer ${styles.backAndForth}`)
     const [laugh, setLaugh] = useState(`${styles.reactionLaughHover} ${styles.translateLaughStart} cursor-pointer ${styles.backAndForth}`)
+    const [awardBackgroundStyles, setAwardBackgroundStyles] = useState(`${styles.awardBackgroundStylesHidden}`)
 
     const [disableLove, setDisableLove] = useState(false)
     const [disableLaugh, setDisableLaugh] = useState(false)
@@ -16,6 +18,9 @@ export default function PostActions({ postMetaData }) {
 
     const handleLoveClick = (e) => {
         e.stopPropagation();
+        setTimeout(() => {
+            setAwardBackgroundStyles(`${styles.awardBackgroundStylesShow}`)
+        }, 300)
         setLaugh("opacity-0")
         setLove(`${styles.translateLoveEnd}`)
         setDisableLaugh(true)
@@ -24,6 +29,9 @@ export default function PostActions({ postMetaData }) {
 
     const handleLaughClick = (e) => {
         e.stopPropagation()
+        setTimeout(() => {
+            setAwardBackgroundStyles(`${styles.awardBackgroundStylesShow}`)
+        }, 300)
         setLaugh(`${styles.translateLaughEnd}`)
         setLove("opacity-0")
         setDisableLove(true)
@@ -33,12 +41,15 @@ export default function PostActions({ postMetaData }) {
     return (
         <div className="display-flex-column justify-between height-100 width-100 padding-5 padding-top-bottom-10">
             <div className="display-flex-row align-center justify-around text-shadow-black height-100 width-100 relative">
-                <button disabled={disableLove} className={`${styles.catLoveContainer} ${love} absolute`} onClick={handleLoveClick}>
-                    <CatLove width={80} height={70} />
+                <button disabled={disableLove} className={`${styles.catLoveContainer} ${love} absolute padding-0`} onClick={handleLoveClick}>
+                    <CatLove width={74} height={64} />
                 </button>
-                <button disabled={disableLaugh} className={`${styles.catLaughContainer} ${laugh} absolute`} onClick={handleLaughClick}>
-                    <CatLaugh width={80} height={70} />
+                <button disabled={disableLaugh} className={`${styles.catLaughContainer} ${laugh} absolute padding-0`} onClick={handleLaughClick}>
+                    <CatLaugh width={74} height={64} />
                 </button>
+                <div className={`${awardBackgroundStyles}`}>
+                    <AwardBackground height={131} />
+                </div>
             </div>
             <div className="padding-left-right-10 absolute">
                 <p className="text-shadow-black color-white">here</p>
