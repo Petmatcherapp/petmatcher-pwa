db62214010eb709f4adfa735d88046b97f653ba0
ryanbranco <ryanbr23@gmail.com>
2021-05-14 18:33:29 -0700

    genderBubble 0.4em -> 0.5em

--- Components/Buttons/Action.js
+++ Components/Buttons/Action.js
@@ -1,3 +1,5 @@
+import styles from "./Action.module.css"
+
 export default function Action({ text, onClick }) {
     const handleClick = (e) => {
         e.preventDefault()
@@ -6,6 +8,6 @@ export default function Action({ text, onClick }) {
     }
 
     return (
-        <p onClick={handleClick} className={`display-inline gradient-text gradient-3 cursor-pointer text-shadow-white weight-700`}>{text}</p>
+        <p onClick={handleClick} className={`${styles.action} display-inline gradient-text gradient-3 cursor-pointer weight-700`}>{text}</p>
     )
 }
