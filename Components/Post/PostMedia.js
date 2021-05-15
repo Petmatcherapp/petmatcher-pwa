6e60a561188e1414a60288ad37431010a32ad42a
ryanbranco <ryanbr23@gmail.com>
2021-05-15 13:09:25 -0700

    video progress + restart button

--- Components/Post/PostMedia.js
+++ Components/Post/PostMedia.js
@@ -3,17 +3,19 @@ import Play from "../Images/Svgs/Icons/Play"
 import Pause from "../Images/Svgs/Icons/Pause"
 import VolumeOn from "../Images/Svgs/Icons/VolumeOn"
 import VolumeMute from "../Images/Svgs/Icons/VolumeMute"
+import Replay from "../Images/Svgs/Icons/Replay"
 import styles from "./PostMedia.module.css"
 
 export default function PostMedia({ postData, visibility }) {
     const videoPlayer = useRef(null)
     const [playing, setPlaying] = useState(false)
     const [mute, setMute] = useState(false)
+    const [videoProgress, setVideoProgress] = useState(0)
 
     const handleLoad = () => {
-        // This is needed because of autoPLay, and 
+        // This is needed because of autoplay, and 
         // autoPlay is needed to show the poster on
-        // IOS (or else its just a white screen)
+        // IOS (or else it's just a white screen)
         // This is a temp solution: see Jira ticket
         // #--- for the better solution
         videoPlayer.current.pause()
@@ -39,12 +41,23 @@ export default function PostMedia({ postData, visibility }) {
         }
     }
 
+    const handleTimeUpdate = () => {
+        setVideoProgress((100 / videoPlayer.current.duration) * videoPlayer.current.currentTime) 
+    }
+
+    const handleRestart = () => {
+        videoPlayer.current.currentTime = 0
+    }
+
     return (
         <>
             {
             postData.srcType === "video" &&
             <>
             <div className={`${visibility} display-flex-column align-end justify-end absolute width-100 height-100`}>
+                <div onClick={handleRestart} className={`${styles.controls} padding-10`}>
+                    <Replay height={40} />
+                </div>
                 <div onClick={handleVolume} className={`${!mute ? "display-block" : "display-none"} ${styles.controls} padding-10`}>
                     <VolumeOn height={40} />
                 </div>
@@ -57,8 +70,9 @@ export default function PostMedia({ postData, visibility }) {
                 <div onClick={handlePlay} className={`${playing ? "display-block" : "display-none"} ${styles.controls} padding-10`}>
                     <Pause height={40} />
                 </div>
+                <div style={{width: `${videoProgress}%`}} className={`${styles.progressBar} gradient-success absolute`}></div>
             </div>
-            <video onLoad={() => handleLoad()} ref={videoPlayer} autoPlay loop playsInline preload="metadata" disablePictureInPicture className="border-radius-10 width-100" src={postData.src} alt={postData.description}></video>
+            <video onLoad={() => handleLoad()} onTimeUpdate={handleTimeUpdate} ref={videoPlayer} autoPlay loop playsInline preload="metadata" disablePictureInPicture className="border-radius-10 width-100" src={postData.src} alt={postData.description}></video>
             </>
             }
             {
